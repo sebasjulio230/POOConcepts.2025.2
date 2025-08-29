b2f532eaf1b24281e4e65f1b9c10836f12dea659
A Klitschko <sebasjulio10@hotmail.com>
2025-08-28 21:38:01 -0500

    add Abstract class employee

---
+++ POO.Concepts/POO.Concepts.Core/Empleyee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.Concepts.Core;
+
+public abstract class Empleyee
+{
+    public int Id
+    {
+        get;
+        set;
+    }
+    public string FirstName
+    {
+        get;
+        set;
+    } = null!;
+
+    public string LastName
+    {
+        get;
+        set;
+    } = null!;
+    public bool IsActive
+    {
+        get;
+        set;
+    }
+
+    public Date HireDate
+    {
+        get;
+        set;
+    } = null!;
+    public Date bornDate
+    {
+        get;
+        set;
+    } = null!;
+    public abstract decimal GetValueToPay();
+
+    public override string ToString() =>
+        $"{Id}\t{FirstName}  {LastName}\n\t" +
+        $"Hire date..: {HireDate}\n\t" +
+        $"Salary.....: {GetValueToPay():C2}";
+
+
+
+} 
+
+
+  
+
+
+
+
+
+
+
+
+
+

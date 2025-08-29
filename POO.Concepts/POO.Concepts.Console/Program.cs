38ceb3429d3625ed22f0064cff33d89857f55025
A Klitschko <sebasjulio10@hotmail.com>
2025-08-28 20:19:27 -0500

    Finish class date

---
+++ POO.Concepts/POO.Concepts.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using POO.Concepts.Core;
+
+try
+{
+    var date1 = new Date(2025,11,30);
+    var date2 = new Date(2025, 12, 31);
+
+
+    Console.WriteLine(date1.ToString());
+    Console.WriteLine(date2.ToString());
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
---
+++ POO.Concepts/POO.Concepts.Core/Date.cs
@@ -0,0 +1,91 @@
+﻿namespace POO.Concepts.Core;
+
+public class Date
+{
+    private int _day;
+    private int _month;  
+    private int _year;
+
+    public Date()
+    {
+        Year = 1900;
+        Month = 1;
+        Day = 1;
+    }
+
+    public Date(int year, int month, int day)
+    {
+        Year = year;
+        Month = month;
+        Day = day;
+    }
+
+    public int Day 
+    {
+        get => _day;
+        set
+        {
+            _day = ValidateDay(value);
+        }
+    }
+
+    public int Month 
+    {
+        get => _month;
+        set
+        {
+            _month = ValidateMonth(value);
+        }
+    }
+
+    public int Year 
+    {
+        get => _year;
+        set
+        {
+            _year = ValidateYear(value);
+        }
+    }
+
+
+    public override string ToString()
+    {
+        return $"{Year:0000}/{Month:00}/{Day:00}";
+    }
+    private bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    private int ValidateYear(int year)
+    {
+        if (year < 0)
+        {
+            throw new Exception("Year cannot be negative.");
+        }
+        return year;
+    }                   
+
+    private int ValidateMonth(int month)
+    {
+        if (month < 1 || month > 12)
+        {
+            throw new Exception($"The month {month}, is not valid");
+        }
+        return month;
+    }
+    
+    private int ValidateDay(int day)
+    {
+        if (Month < 1 || Month > 12)
+            throw new Exception($"The month {Month} is not valid.");
+
+        int[] daysInMonth = { 31, IsLeapYear(Year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int maxDay = daysInMonth[Month - 1];
+
+        if (day < 1 || day > maxDay)
+            throw new Exception($"The day {day} is not valid for month {Month} in year {Year}.");
+
+        return day;
+    }
+}
